158a9424d8decc93b89b833ce71f09f427dcd443
dapoadedire <adedireadedapo19@gmail.com>
2023-03-02 11:45:23 +0100

    ...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
       >
         Shop Now
       </a> */}
-      <a href="#_" className="group relative inline-block text-lg">
+      <button className="group relative inline-block text-lg">
         <span className="relative z-10 block overflow-hidden rounded-md border-2 border-gray-900 px-5 py-3 font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out group-hover:text-white">
           <span className="absolute inset-0 h-full w-full rounded-md bg-gray-50 px-5 py-3"></span>
           <span className="ease absolute left-0 -ml-2 h-48 w-48 origin-top-right -translate-x-full translate-y-12 -rotate-90 bg-gray-900 transition-all duration-300 group-hover:-rotate-180"></span>
@@ -46,7 +46,7 @@ const Hero = () => {
           className="absolute bottom-0 right-0 -mb-1 -mr-1 h-12 w-full rounded-lg bg-gray-900 transition-all duration-200 ease-linear group-hover:mb-0 group-hover:mr-0"
           data-rounded="rounded-lg"
         ></span>
-      </a>
+      </button>
     </div>
   );
 };
